c4b915248e094469525dd4ce9de9259def00b239
«Zhenya» <saltykovazh@mail.ru>
2022-12-21 19:40:39 +0400

    Task06

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Является данное число Четным? ");
+Write("Введите  число a: ");
+int a = int.Parse(ReadLine());
+
+ if (a%2==0)
+ {
+    WriteLine ("Да");
+ }
+ else
+ {
+    WriteLine ("Нет");
+ }
