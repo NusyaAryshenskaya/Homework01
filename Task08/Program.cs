d54b5199f920df9adc987aa6d79855b17d848250
«Zhenya» <saltykovazh@mail.ru>
2022-12-21 20:11:51 +0400

    Task08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Все четные числа от 0 до N? ");
+Write("Введите  число N: ");
+int N = int.Parse(ReadLine());
+int a = 0;
+
+while (a <= N);
+{
+ if (a%2==0) 
+ {
+ WriteLine($"{a} ");
+ a = a++;
+}
+}
