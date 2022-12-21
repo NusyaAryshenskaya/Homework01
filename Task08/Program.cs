ff718a79f5a24df8305f38a79e16f7067054f634
«Zhenya» <saltykovazh@mail.ru>
2022-12-21 20:34:54 +0400

    Task08 added

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -2,14 +2,14 @@
 
 WriteLine("Все четные числа от 0 до N? ");
 Write("Введите  число N: ");
-int N = int.Parse(ReadLine());
+int N = int.Parse(ReadLine()!);
 int a = 0;
 
-while (a <= N);
+while (a <= N)
 {
  if (a%2==0) 
  {
- WriteLine($"{a} ");
- a = a++;
+Write($"{a}, ");
 }
+ a = a+1;
 }
