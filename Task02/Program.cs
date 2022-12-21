cc6ed4519067a5a6b733bc405caef6dd208a3d39
«Zhenya» <saltykovazh@mail.ru>
2022-12-21 19:05:47 +0400

    task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Какое из чисел больше? ");
+Write("Введите  число a: ");
+int a = int.Parse(ReadLine());
+Write("Введите  число b: ");
+int b = int.Parse(ReadLine());
+
+if (a > b)
+{
+    WriteLine(a);
+}
+else
+{
+    WriteLine(b);
+}
